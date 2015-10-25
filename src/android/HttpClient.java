1811da52f64550bde1a70afbe39c696aeebafead
pedox <cungkring.ceking@gmail.com>
2015-10-25 18:46:33 +0700

    update catch error

--- src/android/HttpClient.java
+++ src/android/HttpClient.java
@@ -1,3 +1,21 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
 package com.pedox.plugin.HttpClient;
 
 import org.apache.cordova.CordovaWebView;
@@ -95,12 +113,12 @@ public class HttpClient extends CordovaPlugin {
         client.get(url, new TextHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                HttpClient.handleResult(false, statusCode, headers, responseString, callbackContext);
+                HttpClient.handleResult(false, statusCode, headers, responseString, callbackContext, throwable);
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                HttpClient.handleResult(true, statusCode, headers, responseString, callbackContext);
+                HttpClient.handleResult(true, statusCode, headers, responseString, callbackContext, null);
             }
         });
 
@@ -119,24 +137,29 @@ public class HttpClient extends CordovaPlugin {
         client.post(url, params, new TextHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                HttpClient.handleResult(false, statusCode, headers, responseString, callbackContext);
+                HttpClient.handleResult(false, statusCode, headers, responseString, callbackContext, throwable);
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                HttpClient.handleResult(true, statusCode, headers, responseString, callbackContext);
+                HttpClient.handleResult(true, statusCode, headers, responseString, callbackContext, null);
             }
         });
     }
 
-    private static void handleResult(boolean success, int statusCode, Header[] headers, String responseString, CallbackContext callbackContext)
+    private static void handleResult(boolean success, int statusCode, Header[] headers, String responseString, CallbackContext callbackContext, Throwable throwable)
     {
         JSONObject result = new JSONObject();
         try {
             /** Set header **/
             JSONObject headerParam = new JSONObject();
-            for (Header param : headers) {
-                headerParam.put(param.getName(), param.getValue());
+            if(headers != null) {
+                for (Header param : headers) {
+                    headerParam.put(param.getName(), param.getValue());
+                }
+            }
+            if(throwable != null) {
+                result.put("error", throwable.getMessage());
             }
             result.put("result", responseString);
             result.put("code", statusCode);
